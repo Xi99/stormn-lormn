9427782d7203f9e604d338584d66ea5edc4245d3
Sean <sean.lorman@gmail.com>
2020-04-21 18:39:41 -0600

    Learn different RN syntax, and start to build out framework

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enteredZip, setEnteredZip] = useState("");
+  const [userZips, setUserZips] = useState([]);
+
+  const zipInputHandler = (enteredZip) => {
+    setEnteredZip(enteredZip);
+  };
+
+  const addZipHandler = () => {
+    setUserZips((currentZips) => [
+      ...currentZips,
+      { id: Math.random().toString(), zip: enteredZip },
+    ]);
+  };
+
   return (
-    <View style={{ padding: 50 }}>
+    <View style={styles.screen}>
       <View>
         <TextInput
           placeholder="Enter a Zip"
-          style={{ borderColor: "black", borderBottomWidth: 1 }}
+          style={styles.zip}
+          onChangeText={zipInputHandler}
+          value={enteredZip}
         />
-        <Button title="Get The Weather!" />
+        <Button title="Get The Weather!" onPress={addZipHandler} />
       </View>
-      <View></View>
+      <View style={styles.todaysWeather}>
+        <Text>Weather Goes Here</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 40,
+    borderColor: "red",
+    borderWidth: 2,
+  },
+  todaysWeather: {
+    flex: 2,
+    borderColor: "green",
+    borderWidth: 2,
+  },
+  zip: {
+    flex: 1,
+    borderColor: "black",
+    borderBottomWidth: 1,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
 
 // const [outputText, setOutputText] = useState("Storm'n Lorm'n!");
---
+++ components/FlatList.js
@@ -0,0 +1,10 @@
+{
+  /* <FlatList
+  data={userZips}
+  renderItem={(itemData) => (
+    <View style={styles.listItem}>
+      <Text>{itemData.item.zip}</Text>
+    </View>
+  )}
+/>; */
+}
