8c8941cf2738fcb10c3fb8fa820a7e24ec68efd3
Sean <sean.lorman@gmail.com>
2020-07-28 23:36:37 -0600

    Add test Deno code to make api

---
+++ api/db/database.js
@@ -0,0 +1,22 @@
+import { Client } from "https://deno.land/x/postgres/mod.ts";
+
+class Database {
+  constructor() {
+    this.connect();
+  }
+
+  //TODO: Change to below to fit my db
+  async connect() {
+    this.client = new Client({
+      user: "postgres",
+      database: "logrocket_deno",
+      hostname: "localhost",
+      password: "postgres",
+      port: 5432,
+    });
+
+    await this.client.connect();
+  }
+}
+
+export default new Database().client;
