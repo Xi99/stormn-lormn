cb945aecdb7d6b9227f898b6203fff65ad5207e3
Sean <sean.lorman@gmail.com>
2020-08-13 23:05:21 -0600

    Add root route to show text for testing purposes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ const port = 8080;
 app.use(cors());
 app.use(bp.json());
 // app.get(req, res) ...
+
+app.get("/", (req, res) => {
+  res.send("<h1>Storm'n Lorm'n</h1>");
+});
 app.get("/users", (req, res) => {
   pgdb.getAllUsers().then((results) => res.send(results));
 });
